d2e3a473888e7ec1221dd448adee5c487b584886
Misdar Manto <misdarmanto1922@gmail.com>
2022-12-11 14:49:33 +0700

    deploy

--- public/apps/middlewares/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.middleware = void 0;
-const access_1 = require("./access");
-exports.middleware = { useAuthorization: access_1.useAuthorization };
